dadbc8d8bd95e4782362b84d3613807c5d83e77d
ondrej.certik <ondrej.certik@2c2dc81c-e92d-0410-b1bd-bb3a7c3cceac>
2007-04-16 19:50:53 +0000

    working mobile & phone protocol

--- pyconnect/mobile.py
+++ pyconnect/mobile.py
@@ -1,21 +1,70 @@
-import pickle
+import sys
+import StringIO
+import traceback
 
-class Server:
-    def run(self,s):
-        """Run the "s" that was received over the bluetooth"""
-        o = pickle.loads(s)
+def run(s):
+    saveout = sys.stdout
+    out = StringIO.StringIO()
+    sys.stdout = out
+    try:
         try:
-            if o[0] == 1:
-                #eval(o[1])
-                eval("x=1")
-            raise "wrong protocol"
+            exec s in globals()
         except:
-            #TODO: print more useful info about the exception
-            print "sorry, your code raised an exception"
-            raise
-        print "Done."
+            traceback.print_exc(file=sys.stdout)
+    finally:
+        sys.stdout = saveout
+    return out.getvalue()
 
-code = open("/tmp/x").read()
 
-s = Server()
-s.run(code)
+import socket;
+
+def pickle_str(s):
+    x = "%.12d"%len(s)+s
+    return x
+
+def recv_all(sock,size):
+    """ Recieves a data from the socket of a required size.
+
+    param:
+        size - requested size to recieve.
+    """
+    msg = '';
+    while len(msg) < size:
+        chunk = sock.recv(size-len(msg))
+        if chunk == '':
+            raise RuntimeError, "socket connection broken"
+        msg = msg + chunk
+    return msg
+
+def recv(sock):
+    """ Return an pickled object from socket. """
+
+    # first let recieve only a header.
+    header = recv_all(sock,12)
+
+    # get data size from the header & get the data from socket.
+    datasize = int(header)
+    data = recv_all(sock,datasize)
+
+    return data
+
+
+b = socket.socket(socket.AF_BT,socket.SOCK_STREAM);
+b.bind(("",5))
+b.listen(1)
+while 1:
+    try:
+        print "listening"
+        sock, info =  b.accept()
+        print "connected"
+        print "waiting for a code"
+        x = recv(sock)
+        print "running the code"
+        s = run(x)
+        print "done. sending the result back"
+        sock.sendall(pickle_str(s))
+        print "closing"
+        sock.close()
+        print "done."
+    except:
+        print "exception raised"
--- pyconnect/pc.py
+++ pyconnect/pc.py
@@ -1,25 +1,144 @@
 import pickle
+import sys
+import StringIO
+import traceback
+import time
 
-def f():
-    x = 1
-    print "Man, it works:",x
-    print eval("x")
-    print eval("x", {x: x},{x: x})
+IN_PC = 0
+IN_MOBILE = 1
 
-class Runner(object):
-    def __init__(self):
-        pass
+WHERE = -1
 
-    def run(self, server):
-        print "ok, we are there."
-        print type(server)
+try:
+    import bluetooth;
+    WHERE = IN_PC;
+except ImportError:
+    import socket;
+    WHERE = IN_MOBILE;
 
-r = [1, """print "ok" """]
+class bluesocket:
+    """ The bluetooth socket which works on a pc via the python-bluez 
+    as well as on a mobile via pyS60 sockets. 
+        
+    """
 
-open("/tmp/x","w").write(pickle.dumps(r))
+    def __init__(self,headersize):
+        """ Inits a socket.
 
-f()
+        param:
+            headersize - fixed size of a header in bytes which is sent before each pickled object.
+                         In this header is saved how large is pickled object so a recipient knows
+                         how large data should recive recieve to succesfully unpickle the object. 
+        """
+                         
+        self.headersize = headersize
+        
+        if WHERE == IN_PC:
+           self.socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM);
+        elif WHERE == IN_MOBILE:
+            self.socket = socket.socket(socket.AF_BT,socket.SOCK_STREAM);
 
-#links:
-#http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/437932
-#http://lybniz2.sourceforge.net/safeeval.html
+    def connect(self,addr,channel):
+        self.socket.connect( (addr,channel) )
+
+    def close(self):
+        self.socket.close()
+
+    def accept(self):
+        return self.socket.accept()
+
+    def bind(self,channel):
+        self.socket.bind(("",channel))
+
+    def send(self,obj):
+        """ Sends a pickled object. """
+        # pickkle our object & send its size in a header.
+        data = cPickle.dumps(obj);
+        self._send_header(len(data));
+
+        # send object itself.
+        return self.socket.sendall(data);
+
+    def recv(self):
+        """ Return an pickled object from socket. """
+
+        # first let recieve only a header.
+        header = self._recv_all(self.headersize)
+
+        # get data size from the header & get the data from socket.
+        datasize = int(header)
+        data = self._recv_all(datasize)
+
+        return data;
+
+    def _send_header(self,datasize): 
+        """ Send a header which contains a size of next pickled object. 
+        
+        param:
+            datasize - a size of next pickled object to send.
+        """
+        header = "%%.%dd" % self.headersize 
+        header = header % datasize
+
+        self.socket.sendall(header)
+
+    def _recv_all(self,size):
+        """ Recieves a data from the socket of a required size.
+
+        param:
+            size - requested size to recieve.
+        """
+        msg = '';
+        while len(msg) < size:
+            chunk = self.sock.recv(size-len(msg))
+            if chunk == '':
+                raise RuntimeError, "socket connection broken"
+            msg = msg + chunk
+        return msg
+
+def pickle_str(s):
+    x = "%.12d"%len(s)+s
+    return x
+
+def recv_all(sock,size):
+    """ Recieves a data from the socket of a required size.
+
+    param:
+        size - requested size to recieve.
+    """
+    msg = '';
+    while len(msg) < size:
+        chunk = sock.recv(size-len(msg))
+        if chunk == '':
+            raise RuntimeError, "socket connection broken"
+        msg = msg + chunk
+    return msg
+
+def recv(sock):
+    """ Return an pickled object from socket. """
+
+    # first let recieve only a header.
+    header = recv_all(sock,12)
+
+    # get data size from the header & get the data from socket.
+    datasize = int(header)
+    data = recv_all(sock,datasize)
+
+    return data
+
+b = bluesocket(12)
+print "connecting"
+b.connect("00:19:79:86:EB:BC",5)
+print "connected:"
+print "sending str"
+code=r"""\
+print "This code runs on the phone"
+import os, sys
+print "current dir:",os.getcwd()
+print "python version:"
+print sys.version
+"""
+b.socket.sendall(pickle_str(code))
+print "receiving"
+print recv(b.socket)
+print "stop"
